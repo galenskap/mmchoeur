ebaf6de09b300f2f340ed6d71cf485f5e8655cd0
fabio <fabiocerqueiracw@gmail.com>
2018-08-12 15:29:20 +0200

    Mobile - partothèque

--- sites/all/themes/mmchoeur/src/js/all.js
+++ sites/all/themes/mmchoeur/src/js/all.js
@@ -1,5 +1,14 @@
 jQuery(document).ready(function(){
+    // Handle mobile menu
    jQuery('.toggle-wrapper, .close-menu').on('click',function () {
        jQuery('.main-menu').toggleClass('active');
    });
+   if(jQuery('body').hasClass('mobile')) {
+     // Mobile views filters
+    jQuery('.view-filters').on('click',function (e) {
+        if(jQuery(e.target).hasClass('view-filters')) {
+            jQuery('.view-filters > form').toggleClass('active');
+        }
+    });
+   }
 });
